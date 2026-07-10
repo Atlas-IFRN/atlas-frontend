4e8d73b83c966db0a11e5612b13a4812bf799a8d
Nathan Maniçoba <zaphiroxd5@gmail.com>
2026-07-10 09:59:45 -0300

    feat: adiciona secoes de detalhe da trilha

--- src/components/trilhas/index.ts
+++ src/components/trilhas/index.ts
@@ -1,9 +1,15 @@
 import './TrailsCatalog.css'
 
 export type {
+  TrackLevel,
   Trail,
   TrailAreaLabel,
+  TrailEvaluation,
   TrailFilter,
+  TrailLesson,
+  TrailLessonType,
+  TrailModule,
+  TrailTeacher,
   TrailTheme,
   TrailsStats,
 } from './types'
@@ -12,6 +18,18 @@ export { TrailBanner } from './molecules/TrailBanner'
 export { TrailFooterMeta } from './molecules/TrailFooterMeta'
 export { TrailSkillList } from './molecules/TrailSkillList'
 export { TrailCard } from './organisms/TrailCard'
+export {
+  TrailBreadcrumb,
+  TrailDetailHero,
+  TrailDetailSidebar,
+  TrailDetailTabs,
+  TrailEvaluationCard,
+  TrailModulesPanel,
+  TrailOutcomes,
+  TrailPrerequisites,
+  TrailProfessorBlock,
+  type TrailDetailTab,
+} from './organisms/TrailDetailSections'
 export { TrailsGrid } from './organisms/TrailsGrid'
 export { TrailsHero } from './organisms/TrailsHero'
 export { TrailsStatsGrid } from './organisms/TrailsStatsGrid'
